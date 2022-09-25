3b51576004780ddc0c831b766cee062864de33af
STEMPLE <stemple0@naver.com>
2022-09-25 02:53:01 +0000

    2

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
 import express from "express";
 import morgan from "morgan";
+import globalRouter from "./routers/globalRouter.js";
+import userRouter from "./routers/userRouter.js";
+import videoRouter from "./routers/videoRouter.js";
+import "./db.js";
+import "./models/User.js";
 
 const PORT = "4000";
-
 const app = express();
-
 app.use(morgan("dev"));
-app.get("/");
+app.set("view engine", "pug");
+app.set("views", process.cwd() + "/src/views");
+
+app.use("/", globalRouter);
+app.use("/video", videoRouter);
+app.use("/user", userRouter);
 
 app.listen(PORT, console.log(`@ Server Starting In Port ${PORT} @`));
 //
